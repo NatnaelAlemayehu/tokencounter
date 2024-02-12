30b8929c41f4f0712e73ce0aed950c2a74987427
Natnael <natnaelzewdu0@gmail.com>
2024-02-12 09:46:40 +0300

    changed to default tiktoken module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require('cors');
-const { encodingForModel } = require("js-tiktoken");
+const assert = require("assert")
+const {encoding_for_model} = require("tiktoken");
 var bodyParser = require('body-parser')
 const app = express();
 
@@ -8,20 +9,23 @@ app.use(cors());
 app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+    const enc = encoding_for_model("gpt-4-vision-preview");
+    const ans = enc.encode("Hello World")
+    console.log (ans)
+    res.json({ tokens: ans.length });
 });
 
 app.post('/api/tokens', (req, res) => {
-  const userText = req.body.text;
-  const enc = encodingForModel("gpt-4-vision-preview");
-  const answer = enc.encode(userText);
-  res.json({ tokens: answer.length });
+    const userText = req.body.text;
+    const enc = encodingForModel("gpt-4-vision-preview");
+    const answer = enc.encode(userText);
+    res.json({ tokens: answer.length });
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
